576de68b650e0f8199c997f77536e7b231f71859
Morph Robotics <104451879+MorphRobotics@users.noreply.github.com>
2022-07-21 20:34:45 -0400

    Add files via upload

---
+++ Rock_Paper_Scissors.ino
@@ -0,0 +1,81 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver myServo = Adafruit_PWMServoDriver();
+
+#define SERVOMIN 150
+#define SERVOMAX 600
+long x;
+
+uint8_t servonum = 0;
+uint8_t i = 0;
+uint8_t numberOfServos = 5;
+
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  myServo.begin();
+  myServo.setPWMFreq(40);
+  delay(500);
+}
+
+void loop() {
+  //Random Choice generator
+  x = random(1, 4);
+  //Original fist
+  myServo.setPWM(0, 0, 350);
+
+  myServo.setPWM(1, 0, 150);
+
+  myServo.setPWM(2, 0, 150);
+
+  myServo.setPWM(3, 0, 150);
+
+  myServo.setPWM(4, 0, 150);
+  delay(1000);
+
+  delay(275);
+  Serial.println(x);
+
+//Rock
+  if (x == 1) {
+    myServo.setPWM(0, 0, 350);
+
+    myServo.setPWM(1, 0, 150);
+
+    myServo.setPWM(2, 0, 150);
+
+    myServo.setPWM(3, 0, 150);
+
+    myServo.setPWM(4, 0, 150);
+    delay(1000);
+
+  }
+  //Paper
+  if (x == 2) {
+    myServo.setPWM(0, 0, 150);
+
+    myServo.setPWM(1, 0, 350);
+
+    myServo.setPWM(2, 0, 350);
+
+    myServo.setPWM(3, 0, 350);
+
+    myServo.setPWM(4, 0, 350);
+    delay(1000);
+  }
+
+  //Scissors
+  if (x == 3) {
+    myServo.setPWM(0, 0, 350);
+
+    myServo.setPWM(1, 0, 350);
+
+    myServo.setPWM(2, 0, 350);
+
+    myServo.setPWM(3, 0, 150);
+
+    myServo.setPWM(4, 0, 150);
+    delay(1000);
+  }
+}
